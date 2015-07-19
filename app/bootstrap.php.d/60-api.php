3cd1f4f9041bc170132cc7c5fa215c373f040d47
Simon Vieille <simon@deblan.fr>
2015-07-19 18:24:43 +0200

    API base_uri

--- app/bootstrap.php.d/60-api.php
+++ app/bootstrap.php.d/60-api.php
@@ -3,5 +3,5 @@
 use Gist\Api\Client;
 
 $app['api_client'] = function ($app) {
-    return new Client(['base_uri' => 'http://127.0.0.1:8080/']);
+    return new Client(['base_uri' => 'https://gist.deblan.org/']);
 };
