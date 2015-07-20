2395b78db7a305d4cfd40f7afe0399c89cc4eee8
Simon Vieille <simon@deblan.fr>
2015-07-20 23:38:31 +0200

    issue with cli

--- app/bootstrap.php.d/00-main.php
+++ app/bootstrap.php.d/00-main.php
@@ -6,4 +6,8 @@ $app = Application::getInstance();
 
 $app['root_path'] = __DIR__ . '/../..';
 
-chdir($app['root_path']);
+if (php_sapi_name() !== 'cli') {
+    chdir($app['root_path']);
+} else {
+    set_include_path(get_include_path().PATH_SEPARATOR.$app['root_path']);
+}
