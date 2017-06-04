0060b188427851efcf2f09c2685c9c7b46755860
Simon Vieille <simon@deblan.fr>
2017-06-04 17:27:01 +0200

    API base url setting

--- app/bootstrap.php.d/60-api.php
+++ app/bootstrap.php.d/60-api.php
@@ -3,5 +3,5 @@
 use Gist\Api\Client;
 
 $app['api_client'] = $app->share(function ($app) {
-    return new Client(['base_uri' => $app['settings']['api']['base_uri']]);
+    return new Client(['base_uri' => rtrim($app['settings']['api']['base_url'], '/')]);
 });
